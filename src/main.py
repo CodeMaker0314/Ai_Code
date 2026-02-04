a184fd461bb4ab2a23f4a1ffee7b581fe115edbb
CodeMaker0314 <codemaker0314@gmail.com>
2026-02-04 16:13:23 +0800

    some bug commit

--- src/main.py
+++ src/main.py
@@ -9,9 +9,11 @@ WIDTH, HEIGHT = 480, 480
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ai Train Game")
 
-# Color Initial
+# Color Initial Value
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 clock = pygame.time.Clock()
 running = True
